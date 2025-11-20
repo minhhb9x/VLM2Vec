6f715f6b2d6c38978b1a509eb99d2a275be4bea0
root <minhnguyenquocnhat@gmail.com>
2025-11-20 10:42:01 +0000

    fix distill model

---
+++ debug_scripts/distill/model_debug.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# === Minimal debug version (1 GPU) ===
+
+export EXP_NAME=debug_model
+export EXP_DIR=./runs/$EXP_NAME
+mkdir -p $EXP_DIR
+
+echo "Python: $(which python)"
+echo "Version: $(python --version)"
+
+
+# --model_name Qwen/Qwen2-VL-2B-Instruct \
+
+python src/distillation/model.py \
+  --lora --lora_r 16 \
+  --model_name apple/FastVLM-0.5B \
+  --bf16 --pooling eos --normalize True --temperature 0.02 \
+  --dataset_config debug_scripts/train/train_image_debug.yaml \
+  --image_resolution mid \
+  --resize_use_processor true \
+  --run_name $EXP_NAME --output_dir $EXP_DIR \
+  --learning_rate 5e-5 \
+  --grad_cache False \
+  --per_device_train_batch_size 4 \
+  --gc_q_chunk_size 2 \
+  --gc_p_chunk_size 2 \
+  --interleave_batch_size 4 \
+  --lr_scheduler_type linear \
+  --warmup_steps 5 \
+  --max_steps 20 \
+  --save_steps 10 \
+  --logging_steps 1 \
+  --save_total_limit 2 \
+  --seed 42 \
+  --save_safetensors True \
+  --remove_unused_columns False \
+  --teacher_model_name TIGER-Lab/VLM2Vec-Qwen2VL-2B \
+  --teacher_model_backbone  qwen2_vl \
+  --teacher_pooling eos \
+  --teacher_normalize true \
+  --report_to none 2>&1 | tee $EXP_DIR/train.log
--- src/distillation/model.py
+++ src/distillation/model.py
@@ -2,13 +2,16 @@ import logging
 import os.path
 import sys
 
+PROJECT_ROOT = "/workspace/VLM2Vec"
+sys.path.append(PROJECT_ROOT)
+
 from typing import Dict
 import torch
 import torch.distributed as dist
 from torch import nn, Tensor
-from transformers import HfArgumentParser
+from transformers import HfArgumentParser, AutoConfig
 
-from src.arguments import ModelArguments, DataArguments, TrainingArguments, TeacherArguments
+from src.arguments import ModelArguments, DataArguments, TrainingArguments, TeacherArguments, map_teacher_to_model_args
 from src.model.processor import load_processor, get_backbone_name
 from src.utils.basic_utils import print_rank, print_master, find_latest_checkpoint
 from src.model.model import MMEBModel
@@ -23,21 +26,22 @@ class DistillationModel(nn.Module):
     def __init__(self, student_args: ModelArguments, teacher_args: ModelArguments, data_args: DataArguments, training_args: TrainingArguments):
         super(DistillationModel, self).__init__()
         self.student_args = student_args
-        self.teacher_args = teacher_args
+        self.teacher_args = map_teacher_to_model_args(teacher_args)
         self.data_args = data_args
         self.training_args = training_args
+
         self._load()
     
     def _load(self):
 
         if self.training_args.resume_from == 'auto':
             resume_checkpoint_dir = find_latest_checkpoint(self.training_args.output_dir)
-        if resume_checkpoint_dir:
-            logger.info(f"Resuming from checkpoint: {resume_checkpoint_dir}")
+            if resume_checkpoint_dir:
+                logger.info(f"Resuming from checkpoint: {resume_checkpoint_dir}")
         elif self.training_args.resume_from.isdigit():
             resume_checkpoint_dir = os.path.join(self.training_args.output_dir, f'checkpoint-{self.training_args.resume_from}')
-        if os.path.exists(resume_checkpoint_dir):
-            logger.info(f"Resuming from checkpoint: {resume_checkpoint_dir}")
+            if os.path.exists(resume_checkpoint_dir):
+                logger.info(f"Resuming from checkpoint: {resume_checkpoint_dir}")
         else:
             resume_checkpoint_dir = None
             logger.info("No checkpoint found. Starting fresh training.")
@@ -45,27 +49,32 @@ class DistillationModel(nn.Module):
         self.resume_checkpoint_dir = resume_checkpoint_dir
         logger.info(f"Set resume_checkpoint_dir to: {self.resume_checkpoint_dir}")
 
-
-        self.student_processor = load_processor(self.student_args, self.data_args)
-        self.teacher_processor = load_processor(self.teacher_args, self.data_args)
-
         self.student = MMEBModel.build(self.student_args)
-        self.teacher = MMEBModel.load(self.teacher_args, is_trainable=False, processor=teacher_processor)
-
         student_model_backbone = get_backbone_name(hf_config=self.student.config)
-        teacher_model_backbone = get_backbone_name(hf_config=self.teacher.config)
-
+        setattr(self.training_args, 'student_model_backbone', student_model_backbone)
         setattr(self.student_args, 'model_backbone', student_model_backbone)
-        setattr(self.teacher_args, 'model_backbone', teacher_model_backbone)
-        setattr(self.training_args, 'model_backbone', student_model_backbone)
+        self.student_processor = load_processor(self.student_args, self.data_args)
+        setattr(self.student, 'processor', self.student_processor)
+
+
+        hf_config = AutoConfig.from_pretrained(self.teacher_args.model_name, trust_remote_code=True)
+        if not getattr(self.teacher_args, "model_backbone", None):
+            teacher_model_backbone = get_backbone_name(hf_config=hf_config, model_type=self.teacher_args.model_type)
+            setattr(self.teacher_args, 'model_backbone', teacher_model_backbone)
+        
+        teacher_model_backbone = self.teacher_args.model_backbone
+        setattr(self.training_args, 'teacher_model_backbone', teacher_model_backbone)
+        self.teacher_processor = load_processor(self.teacher_args, self.data_args)
+        self.teacher = MMEBModel.load(self.teacher_args, is_trainable=False, processor=self.teacher_processor)
+        setattr(self.teacher, 'processor', self.teacher_processor)
 
         print_rank(f'student_model_backbone: {student_model_backbone}')
         print_rank(f'teacher_model_backbone: {teacher_model_backbone}')
+        print_rank(f'type of student processor: {type(self.student_processor)}')
+        print_rank(f'type of teacher processor: {type(self.teacher_processor)}')
+        print_rank(f'type of student model: {type(self.student.encoder)}')
+        print_rank(f'type of teacher model: {type(self.teacher.encoder)}')
 
-        student_processor = load_processor(self.student_args, self.data_args)
-        setattr(self.student, 'processor', student_processor)
-        teacher_processor = load_processor(self.teacher_args, self.data_args)
-        setattr(self.teacher, 'processor', teacher_processor)
 
 if __name__ == "__main__":
     parser = HfArgumentParser((ModelArguments, TeacherArguments, DataArguments, TrainingArguments))
